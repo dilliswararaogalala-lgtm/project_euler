334c1fafb6e1f796e764491d28f355f34f7ab0d9
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-08 22:03:52 +0530

    solved problem zero

--- problem_zero.js
+++ problem_zero.js
@@ -1,11 +1,11 @@
 const sqr = (x) => x * x;
 
-const isOdd = x => x % 2n !== 0n;
+const isOdd = x => x % 2 !== 0;
 
 let sum = 0;
 for (let i = 1; i <= 494000; i++) {
   const square = sqr(i);
-  if (isOdd(i) && isOdd(square)) sum += square;
+  if (isOdd(i)) sum += square;
 }
 
-console.log(sum.toString());
+console.log(sum);
